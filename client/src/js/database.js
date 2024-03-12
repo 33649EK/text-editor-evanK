644ba9ad5d6c8e4ae1faaad13a81a79fc53c126e
Evan Keller <336493k@gmail.com>
2024-03-12 15:35:22 -0600

    modified database.js and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const connectDb = await openDB('jate', 1);
+  const tx = connectDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.add({ content: content });
+  const result = await req;
+  console.log(`Result: ${result}`);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const connectDb = await openDB('jate', 1);
+  const tx = connectDb.transaction('jate', 'readonly');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.getAll();
+  const result = await req;
+  console.log('getResult:', result);
+  return result;
+};
 
 initdb();
